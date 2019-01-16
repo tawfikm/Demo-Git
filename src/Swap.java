9091a9c6d5dae5a2efbb998cd98a9fc02f51899a
tawfikm <tawfikmourchid@gmail.com>
2019-01-16 19:23:38 +0100

    first commit made by Tawfik

---
+++ src/Swap.java
@@ -0,0 +1,22 @@
+
+public class Swap {
+
+    public static  void  main(String args[]){
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+
+        // swapping
+        int temp = x;
+        x = y;
+        y = temp;
+
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+
+    }
+}
